2e76376d0c198c0ba68f95dbece86af91660794e
Justinas Sinkevicius <justusss@gmail.com>
2022-02-01 18:40:37 +0200

    form comp upd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Test from './components/Test';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
-      <Test/>
+      <Form/>
     </div>
   );
 }
---
+++ src/components/Form.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Form = () => {
+    return <div>
+        <input type="text" placeholder='input' />
+        <button>Send</button>
+    </div>;
+};
+
+export default Form;
